53fea127c1b2cd15f33edbb04355910e38393afc
sudosurmrf <145930761+sudosurmrf@users.noreply.github.com>
2024-08-02 14:54:09 -0700

    updated client.cjs for deployable use

--- db/client.cjs
+++ db/client.cjs
@@ -1,5 +1,6 @@
 const { Client } = require('pg');
 const { username, passw } = require('./credentials.cjs')
-const client = new Client(process.env.DATABASE_URL || `postgres://${username}:${passw}@localhost:5432/acme_employee`);
+// const client = new Client(process.env.DATABASE_URL || `postgres://${username}:${passw}@localhost:5432/acme_employee`);
+const client = new Client(process.env.DATABASE_URL || `postgres://localhost:5432/acme_employee`);
 
 module.exports = client;
